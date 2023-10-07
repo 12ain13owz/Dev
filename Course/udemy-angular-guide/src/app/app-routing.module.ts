dd8eb12f4cfc62cd4079fc1bdc611f4601c2bbeb
Dryst <touchfn@gmail.com>
2023-10-07 18:34:53 +0700

    Update course angular
    Lesson 12

--- Course/udemy-angular-guide/src/app/app-routing.module.ts
+++ Course/udemy-angular-guide/src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { ProjectModule } from './project/project.module';
 import { DataBindingModule } from './data-binding/data-binding.module';
 import { DirectiveModule } from './directive/directive.module';
 import { ServicesModule } from './services/services.module';
+import { RoutingStartModule } from './routing-start/routing-start.module';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'project', pathMatch: 'full' },
+  // { path: '', redirectTo: 'project', pathMatch: 'full' },
 
   { path: 'project', loadChildren: () => ProjectModule },
   { path: 'server', loadChildren: () => MainServerModule },
   { path: 'binding', loadChildren: () => DataBindingModule },
   { path: 'directive', loadChildren: () => DirectiveModule },
   { path: 'services', loadChildren: () => ServicesModule },
+  { path: 'routing', loadChildren: () => RoutingStartModule },
 ];
 
 @NgModule({
