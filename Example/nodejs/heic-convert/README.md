1d34edecf4ae5fe55d00b242f216c8834fb8b1de
Dryst <touchfn@gmail.com>
2023-11-30 23:42:15 +0700

    fix bug

--- Example/nodejs/heic-convert/README.md
+++ Example/nodejs/heic-convert/README.md
@@ -1,3 +1,9 @@
 # HEIC to png Coverter
 
-This is a simple command-line tool for converting HEIC files to PNG format. It processes HEIC files located in the `images` folder and saves the converted PNG files to the `uploads` folder.
+This is a simple command-line tool for converting HEIC files to PNG format. It processes HEIC files located in the `heic_images` folder and saves the converted PNG files to the `png_images` folder.
+
+### Run
+
+```
+npm start
+```
--- Example/nodejs/heic-convert/index.js
+++ Example/nodejs/heic-convert/index.js
@@ -2,27 +2,33 @@ const fs = require("fs");
 const path = require("path");
 const convert = require("heic-convert");
 const progress = require("progress");
-const dirImages = path.join(__dirname, "images");
-const dirUploads = path.join(__dirname, "uploads");
+const dirHeicImages = path.join(__dirname, "heic_images");
+const dirPngImages = path.join(__dirname, "png_images");
 
-const files = fs.readdirSync(dirImages);
+const files = fs.readdirSync(dirHeicImages);
 let bar = new progress(":percent", { total: files.length });
 
 const convertBuffer = async () => {
   for (let i = 0; i < files.length; i++) {
-    const filePath = path.join(dirImages, files[i]);
+    const filePath = path.join(dirHeicImages, files[i]);
     const fileName = files[i].split(".")[0];
-    const fileUpload = `${path.join(dirUploads)}/${fileName}.png`;
+    const fileUpload = `${path.join(dirPngImages)}/${fileName}.png`;
     const inputBuffer = fs.readFileSync(filePath);
-    const outputBuffer = await convert({
-      buffer: inputBuffer,
-      format: "PNG",
-    });
 
-    fs.writeFileSync(fileUpload, outputBuffer);
-    bar.tick();
-    if (bar.complete) console.log("complete");
+    try {
+      const outputBuffer = await convert({
+        buffer: inputBuffer,
+        format: "PNG",
+      });
+
+      fs.writeFileSync(fileUpload, outputBuffer);
+      bar.tick();
+      if (bar.complete) console.log("complete");
+    } catch (error) {
+      fs.writeFileSync(fileUpload, inputBuffer);
+      bar.tick();
+    }
   }
 };
 
-//convertBuffer();
+convertBuffer();
