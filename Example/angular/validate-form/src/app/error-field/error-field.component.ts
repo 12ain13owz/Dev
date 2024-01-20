4b442ddbf33d81304f727d6b8074a63256f98245
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-01-20 10:49:35 +0700

    Update angular
    Example validate and reset form

---
+++ Example/angular/validate-form/src/app/error-field/error-field.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'error-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-field.component.html',
+  styleUrl: './error-field.component.scss',
+})
+export class ErrorFieldComponent {}
