00f990c5eb85628a6f31e956e91efda473061894
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-03-22 14:33:36 +0700

    Example nodejs
    auth-jwt
    - Fix Config
    - Change field name

--- Example/nodejs/auth-jwt/config/default.js
+++ Example/nodejs/auth-jwt/config/default.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 module.exports = {
   PRIVATE_KEY: process.env.PRIVATE_KEY,
-  PUBLIC_KEY: process.env.PRIVATE_KEY,
+  PUBLIC_KEY: process.env.PUBLIC_KEY,
 };
--- Example/nodejs/auth-jwt/index.js
+++ Example/nodejs/auth-jwt/index.js
@@ -1,11 +1,11 @@
 const config = require("config");
 const jwt = require("jsonwebtoken");
 
-const privateKey = config.get("PRIVATE_KEY");
-const publicKey = config.get("PUBLIC_KEY");
+const rsaPrivateKey = config.get("PRIVATE_KEY");
+const rasPublicKey = config.get("PUBLIC_KEY");
 
-const token = jwt.sign({ foo: "12345" }, privateKey, { algorithm: "RS256" });
-jwt.verify(token, publicKey, function (err, decoded) {
+const token = jwt.sign({ foo: "bar" }, rsaPrivateKey, { algorithm: "RS256" });
+jwt.verify(token, rasPublicKey, function (err, decoded) {
   if (err) {
     console.log("Error:", err.message);
   } else {
